8a634b223cebe2d2b13501b18b2973b755f542cb
HimikaP <prabhat.himika@gmail.com>
2022-11-11 17:13:51 +0530

    some code

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,44 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.20;
 
-contract Election {
 
-    struct candidate{
-        uint id; 
+
+contract Election {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
         string name;
         uint voteCount;
     }
 
-    //can be used to look up candidates using their ids
-    mapping(uint=>candidate) public candidates;
-
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+    // Store Candidates
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
     uint public candidatesCount;
-    
-function addCandidate (string memory _name) private {
-        candidatesCount ++; // increment no. of candidates
 
-        //storing the data in id-struct mapping
-        candidates[candidatesCount] = candidate(candidatesCount, _name, 0);
-    }
-
-    // to keep track of accounts that have voted
-    mapping(address =>bool ) public voters;
-
-     function vote (uint _candidateId) public {
-   
-        require(!voters[msg.sender]);
-  
-        require(_candidateId > 0 && _candidateId <= candidatesCount);
-    
-        voters[msg.sender] = true; // to ensure that that particular address has voted in the voters mapping
-
-        candidates[_candidateId].voteCount ++;
-    }
+    // voted event
+    event votedEvent (
+        uint indexed _candidateId
+    );
 
 constructor() public {
         addCandidate("Candidate 1");
         addCandidate("Candidate 2");
-       
     }
 
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
+
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender] , "something bad happened");
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+
+        // trigger voted event
+        emit votedEvent(_candidateId);
+    }
+}
